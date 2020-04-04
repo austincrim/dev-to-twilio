68b3bbe0183a206440171d49deaf4aa65c689bec
Austin <aust.crim@gmail.com>
2020-04-04 14:22:15 -0500

    basic incoming text response

--- src/receive_sms.js
+++ src/receive_sms.js
@@ -1,4 +1,20 @@
-import send_text from './send_sms';
+const express = require("express");
+const bodyParser = require("body-parser");
+const MessagingResponse = require("twilio").twiml.MessagingResponse;
 
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 
-send_text("Here are your stats! /n", "+15152037127");
+app.post("/sms", (req, res) => {
+    const twiml = new MessagingResponse();
+
+    console.log(req.body);
+    twiml.message(`Hello, ${req.body.Body}!`);
+
+    res.writeHead(200, { "Content-Type": "text/xml" });
+    res.end(twiml.toString());
+});
+
+app.listen(1337, () => {
+    console.log("Express server listening on port 1337");
+});
--- src/send_sms.js
+++ src/send_sms.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+
 const client = require("twilio")(
   process.env.ACCOUNT_SID,
   process.env.AUTH_TOKEN
