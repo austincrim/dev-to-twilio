3533d3c7b388c0d8aa6fccb0822cd3c35d1257dc
Austin Crim <crim.austin@principal.com>
2020-04-06 14:26:49 -0500

    split recipes into separate messages

--- src/receiveSms.js
+++ src/receiveSms.js
@@ -11,29 +11,21 @@ const PORT = process.env.PORT || 1337;
 app.post("/sms", async (req, res) => {
     const ingredients = req.body.Body;
     console.info(`Ingredients: ${ingredients}`);
-    
-    let message, imageUrls;
+    const twimlResponse = new MessagingResponse();
+
     try {
         const recipeIds = await recipeClient.getRecipeIdsByIngredients(ingredients);
         const recipeDetails = await recipeClient.getRecipeDetailsByIds(recipeIds);
-        const recipeResponseData = recipeDetails.map(r => {
-            return {url: r.sourceUrl, title: r.title, image: r.image}
+        recipeDetails.forEach(r => {
+            const message = twimlResponse.message();
+            message.body(`\n${r.title}\n${r.url}`);
+            message.media(r.image); 
         });
-        imageUrls = recipeResponseData.map(r => r.image);
-        console.log(imageUrls);
-        const responseStrings = recipeResponseData.map(r => `${r.title}\n${r.url}\n\n`);
-        message = `Here are your recipes!\n\n${responseStrings.map(s => s).join('')}`; 
     } catch (error) {
         console.error(error.message);
         message = `Woops! Looks like we had some trouble with that request.\nEnsure that you send a list of ingredients separated by commas (e.g. carrots, rice, chicken).`
     }
     
-    const twimlResponse = new MessagingResponse();
-    const twimlMessage = twimlResponse.message()
-    // TODO add images
-    twimlMessage.body(message);
-    imageUrls.forEach(url => twimlMessage.media(url));
-    console.log(twimlResponse.toString());
     res.writeHead(200, { "Content-Type": "text/xml" });
     res.end(twimlResponse.toString());
 });
